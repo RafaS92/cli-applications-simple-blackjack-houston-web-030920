8d11ac7310da44d0a75bf1d5ea2906a64ce194b1
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-05 04:30:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,11 +32,16 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "h"
-    deal_car
+    card_total += deal_card
+  elsif user_input == "s"
+  card_total
+else 
+  invalid_command
+  prompt_user
 end
 
 def invalid_command
-  # code invalid_command here
+ puts
 end
 
 #####################################################
